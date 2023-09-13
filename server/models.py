31cc46c351c401b90e6094db8a9907ba5323c3bb
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-09-13 14:17:01 -0700

    lab complete

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates("name")
+    def validate_name(self,key,name):
+        if name=="":
+            raise ValueError("Please Enter Name")
+        return name
+
+    @validates("phone_number")
+    def validate_phone(self,key,phone_number):
+        if len(phone_number)!= 10:
+            raise ValueError("Please enter Phone Number")
+        return phone_number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates("content")
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be 250 characters")
+        return content
+    
+    @validates("summary")
+    def validate_summary(self,key,summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary must be 250 characters")
+        return summary
+
+    @validates("category")
+    def validate_category(self,key,category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category is not in either Fiction or Non-Fiction")
+        return category
+    
+    @validates("title")
+    def validates_title(self, key, title):
+        if title not in ["Won't Believe", "Secret", "Top", "Guess"]:
+            raise ValueError("Title is incorrect")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
